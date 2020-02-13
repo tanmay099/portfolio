42ed04e1944cde9a22f4fe02ac7561ba9b8dc419
Tanmay Sharma <tanmay1844.sharma@gmail.com>
2020-02-13 17:45:29 +0530

    log added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var http = require('http');
+var http = require("http");
 
-var nStatic = require('node-static');
+var nStatic = require("node-static");
 
-var fileServer = new nStatic.Server('./dist');
+var fileServer = new nStatic.Server("./dist");
 
-http.createServer(function (req, res) {
-    
+http
+  .createServer(function(req, res) {
     fileServer.serve(req, res);
-
-}).listen(5000);
+  })
+  .listen(5000, () => console.log("server is up and running"));
