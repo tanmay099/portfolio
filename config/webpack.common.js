bbd856076fdc03b2093f7ffc672623528a9e0a11
Tanmay Sharma <tanmay1844.sharma@gmail.com>
2020-04-24 00:05:08 +0530

    error fix

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+// .BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./src/index.js",
